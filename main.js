a39e3762facf69d203653ba4ae2a54a646a4dc75
BingoBlastGAME <bonpaulom@yahoo.com>
2014-07-12 17:29:38 +0800

    qwe

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello Andrei, I've been expecting you.');
+  res.send('Hello Andrei, Ive been expecting you.');
 });
 
 var port = Number(process.env.PORT || 5000);
